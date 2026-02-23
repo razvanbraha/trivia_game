517bb38c265a3b05165fa98c788f87fd90822573
Connor Hekking <clhekkin@ncsu.edu>
2026-02-23 10:44:52 -0500

    remove cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
 
 const app = express();
 const PORT = 8080;
 
-app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../frontend")));
