360ed9c4c4eca70f4a4830a35b6028015d6bf5f0
Riley Wickens <rkwicken@ncsu.edu>
2026-02-24 18:30:48 -0500

    Questions page added to Frontend

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,7 +1,16 @@
+
 function studentMenu() {
     window.location.href = "../html/student-menu.html";
 }
 
 function loginAsProfessor() {
     window.location.href = "../html/teacher-login.html";
+}
+
+function questionsAdd() {
+    globalThis.location.href = "../html/question-add.html";
+}
+
+function home() {
+    globalThis.location.href = "../html/index.html";
 }
