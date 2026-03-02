2415adddac6c0e17c36b29c3e25d48fb86e793d4
razvanbraha <brhrazvan@gmail.com>
2026-03-02 00:08:53 -0500

    refactored the admin db to use promises instead

--- backend/database/admin-db.js
+++ backend/database/admin-db.js
@@ -1,55 +1,42 @@
 const path = require('node:path');
-let mysql = require('mysql2');
+let mysql = require('mysql2/promise');
 require('dotenv').config({ path: path.join(__dirname, '../../.env')})
 
  //Setup initial sql connection
-let con = mysql.createConnection({
-    //Will need user account on vm 
-    host: process.env.host,
-    user: process.env.user, 
-    password: process.env.password,
-});
-
-//Connect to mySQL (no database)
-con.connect(function(err) {
-    if (err) {
-        console.error(err.stack);
-        return;
-    }
-    console.log("Connected!");
-})
+let con; 
 
 /**
  * Setup Admins database & table if none exists.
  * @author Riley Wickens & Razvan Braha
  * @throws {err} if connection/query fails
  */
-const setupAdmins = () => {
-    con.query("CREATE DATABASE IF NOT EXISTS trivia_admins", function (err, result) {
-        if (err) {
-            console.error(err.stack);
-            return;
-        }
-        console.log("Database created");
-    })
+const setupAdmins = async () => {
+
+    // Create database if needed
+    const rootCon = await mysql.createConnection({
+        host: process.env.host, 
+        user: process.env.user,
+        password: process.env.password
+    });
+    await rootCon.query('CREATE DATABASE IF NOT EXISTS trivia_admins');
+    await rootCon.end();
+
     //Connect to mySQL (with database)
-    con = mysql.createConnection({
+    con = await mysql.createConnection({
         //Will need user account on vm 
         host: process.env.host,
         user: process.env.user, 
         password: process.env.password,
         database: process.env.database
     });
-    let qry =`CREATE TABLE IF NOT EXISTS admins (
+
+    // Create table if it doesn't exist
+    const createTableSql = 
+        `CREATE TABLE IF NOT EXISTS admins (
         adminID INT AUTO_INCREMENT PRIMARY KEY,
         unityID VARCHAR(255) NOT NULL)`
-    con.query(qry, function (err, result) {
-        if (err) {
-            console.error(err.stack);
-            return;
-        }
-        console.log("Table Created");
-    })
+
+    await con.query(createTableSql);
 }
 
 /**
@@ -58,19 +45,14 @@ const setupAdmins = () => {
  * @param {Array} body - Array with data to be added to db
  * @throws {err} if connection/query fails
  */
-const addAdmin = (body) => {
+const addAdmin = async (body) => {
     const {unityID} = body;
 
     let data = [unityID];
     let qry = `INSERT INTO admins (unityID) VALUES (?)`;
 
-    con.query(qry, data, function (err, result) {
-        if (err) {
-            console.error(err.stack);
-            return;
-        }
-        console.log("Admin added");
-    })
+    const [result] = await con.query(qry, data);
+    return result.insertId;
 }
 
 /**
@@ -80,7 +62,7 @@ const addAdmin = (body) => {
  * @param {Number} id - Admin ID of admin to update
  * @throws {err} if connection/query fails
  */
-const updateAdmin = (body, id) => {
+const updateAdmin = async (body, id) => {
     const {unityID} = body;
 
     let data = [unityID];
@@ -88,13 +70,8 @@ const updateAdmin = (body, id) => {
         unityID = ${unityID},
         WHERE adminID = ${id}`;
 
-    con.query(qry, data, function (err, result) {
-        if (err) {
-            console.error(err.stack);
-            return;
-        }
-        console.log(`Admin ${id} updated`);
-    })
+    const [result] = await con.query(qry, data);
+    return result.insertId;
 }
 
 /**
@@ -103,15 +80,11 @@ const updateAdmin = (body, id) => {
  * @param {Number} id - Question ID of question to delete
  * @throws {err} if connection/query fails
  */
-const deleteAdmin = (id) => {
-    let qry = `DELETE FROM admins WHERE adminID = ${id}`;
-    con.query(qry, function (err, result) {
-    if (err) {
-        console.error(err.stack);
-        return;
-    }
-        console.log(result);
-    })
+const deleteAdmin = async (id) => {
+    let qry = `DELETE FROM admins WHERE adminID = ?`;
+    let [result] = await con.query(qry, [id]);
+
+
 }
 
 /**
@@ -120,15 +93,10 @@ const deleteAdmin = (id) => {
  * @throws {err} if connection/query fails
  */
 //TODO: Return GET
-const getAllAdmins = () => {
+const getAllAdmins = async () => {
     let qry = `SELECT * FROM admins`;
-    con.query(qry, function (err, result) {
-    if (err) {
-        console.error(err.stack);
-        return;
-    }
-        console.log(result);
-    })
+    const [result] = await con.query(qry);
+    return result;
 }
 
 /**
@@ -138,15 +106,10 @@ const getAllAdmins = () => {
  * @throws {err} if connection/query fails
  */
 //TODO: Return GET
-const getByUnityId = (unityId) => {
-    let qry = `SELECT * FROM admins WHERE unityID = ${unityId}`;
-    con.query(qry, function (err, result) {
-    if (err) {
-        console.error(err.stack);
-        return;
-    }
-        console.log(result);
-    })
+const getByUnityId = async (unityId) => {
+    let qry = `SELECT * FROM admins WHERE unityID = ?`;
+    const [result] = await con.query(qry, [unityId]);
+    return result;
 }
 
 /**
@@ -156,15 +119,9 @@ const getByUnityId = (unityId) => {
  * @throws {err} if connection/query fails
  */
 //TODO: Return GET
-const getByID = (id) => {
-    let qry = `SELECT * FROM admins WHERE adminID = ${id}`;
-    con.query(qry, function (err, result) {
-    if (err) {
-        console.error(err.stack);
-        return;
-    }
-        console.log(result);
-    })
+const getByID = async (id) => {
+    let qry = `SELECT * FROM admins WHERE adminID = ?`;
+    const [result] = await con.query(qry, [id]);
 }
 
 module.exports = {
