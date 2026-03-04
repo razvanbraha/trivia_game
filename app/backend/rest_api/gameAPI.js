8ac682c1c8fad6efd7fe0ed10c804127022281e2
Will Mungas <wrmungas@ncsu.edu>
2026-03-04 11:12:25 -0500

    [TG] add route handling files, incorporate to server

---
+++ app/backend/rest_api/gameAPI.js
@@ -0,0 +1,17 @@
+//-----------------------------------------------------------------------------
+/**
+ * @file gameAPI.js
+ * 
+ * @author Will Mungas, Connor Hekking
+ * 
+ * Handles game-related api routes. These give the user access to the following
+ * functionality:
+ * - initializing new games
+ * - joining existing games
+ * 
+ * After calls to these routes, the user will be upgraded to a WebSocket 
+ * connection for live communication. This uses our own simple protocol, which
+ * is handled/implemented in files under 'app/backend/game/'. 
+ */
+//-----------------------------------------------------------------------------
+
