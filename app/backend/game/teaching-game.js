ceb573d0a74ae6f4dfdc45541b2ffc3a71da5163
Connor Hekking <clhekkin@ncsu.edu>
2026-03-08 16:51:14 -0400

    Change TG constants to match agreed flow

--- app/backend/game/teaching-game.js
+++ app/backend/game/teaching-game.js
@@ -17,13 +17,28 @@
 
 //--- CONSTANTS ---------------------------------------------------------------
 
+const messages = {
+// See API Endpoint Design - https://docs.google.com/document/d/12ijNjsGuGOg7Xqv12Mo_Kprgvlrn7opl86L4AKaVjaU/edit?usp=sharing
+    INIT: 1,
+    START: 2,
+    QUESTION: 3,
+    CHOICES: 4,
+    READY: 5, 
+    ANSWER: 6,
+    CLOSE: 7, 
+    CONTINUE: 8,
+    RESULTS: 9,
+    DONE: 10,
+}
+
 const states = {
     LOBBY: 1, // lobby state - awaiting game start 
     SHOW_QUESTION: 2, // state of each turn when game reveals the question
     SHOW_ANSWERS: 3, // state of each turn when game reveals answer choices
     RECEIVE_RESPONSES: 4, // state of each turn when game accepts responses
-    AWAIT_NEXT: 5, // state of each turn when game waits for teacher to proceed
-    DONE: 6, // final state: close out all data (maybe unnecessary)
+    AWAIT_NEXT: 5, // state of each turn when game waits for teacher to proceed & shows player progress
+    FINAL: 6, // final state: game shows final results & waits for game to end
+    // After final state, game is closed out
 }
 
 //--- GLOBALS -----------------------------------------------------------------
