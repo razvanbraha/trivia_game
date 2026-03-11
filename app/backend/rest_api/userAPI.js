00ec183f138ce9b79cd7c81394cbcedf7457c1de
Will Mungas <wrmungas@ncsu.edu>
2026-03-11 08:05:59 -0400

    [TG] fix typo in useAPI

--- app/backend/rest_api/userAPI.js
+++ app/backend/rest_api/userAPI.js
@@ -24,7 +24,7 @@ const {
 
 const router = express.Router();
 router.use(express.json());
-router.use(express.urlencoded({ extended: true }));k
+router.use(express.urlencoded({ extended: true }));
 
 /*
 
