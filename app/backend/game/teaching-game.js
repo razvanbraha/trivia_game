5f1fd739f7b95ec518b8c288ba49881dcc7e5e0e
Connor Hekking <clhekkin@ncsu.edu>
2026-03-11 13:53:59 -0400

    complete teaching-game.js

--- app/backend/game/teaching-game.js
+++ app/backend/game/teaching-game.js
@@ -44,6 +44,12 @@ class teachingGame {
     answers = {}; // Dict{socket(player): List(answer #)} - list indicies correspond to questions list
     // -1 = no answer
     points = {}; // Dict{socket(player): Number}
+    current_correct_answer_number = -1; // Correct answer number (1/2/3/4)
+    
+    answering_start_time = null; // Date() of the time when the answering period/"live time" started
+
+    signalContinue = null; // Function to resolve the promise making the main game flow(serveQuestions) wait for CONTINUE
+    // Effectively, calling signalContinue() allows the main flow to continue with the next question
 
     // Game settings 
     MAX_QUESTIONS = 50;
@@ -55,6 +61,8 @@ class teachingGame {
     MIN_DEAD_TIME = 0;
     MAX_LIVE_TIME = 30;
     MIN_LIVE_TIME = 1;
+    NO_ANSWER_NUM = -1;
+    BASE_QUESTION_POINTS = 1000;
 
     num_questions = 0;
     categories = [];
@@ -66,7 +74,7 @@ class teachingGame {
     //--- FUNCTIONS ---------------------------------------------------------------
 
     constructor(data) {
-        initTeachingSession(data);
+        this.initTeachingSession(data);
     }
 
     /**
@@ -77,8 +85,7 @@ class teachingGame {
     initTeachingSession(data) {
         this.code = data.game_code;
         this.type = data.game_type;
-        this.state = STATES.LOBBY;
-        this.host.handler = this.receiveMessage;
+        this.state = this.STATES.LOBBY;
     }
 
     sendAllPlayers(message) {
@@ -94,14 +101,13 @@ class teachingGame {
      * @param {Object} message Message object containing the request
      */
     receiveMessage(socket, message) {
-        // TODO other validation?
         try {
             const type = message.type;
 
             // Handle all messages
             switch(type) {
                 case (messages.START):
-                    if (this.state === STATES.LOBBY && socket === this.host) {
+                    if (this.state === this.STATES.LOBBY && socket === this.host) {
                         this.startGame(socket, message);
                     } else if(socket !== this.host) {
                         sendError(socket, "Only host can contiinue.");
@@ -110,7 +116,7 @@ class teachingGame {
                     }
                     break;
                 case (messages.ANSWER):
-                    if (this.state === STATES.RECEIVE_RESPONSES && socket !== this.host) {
+                    if (this.state === this.STATES.RECEIVE_RESPONSES && socket !== this.host) {
                         this.registerAnswer(socket, message);
                     } else if(socket === this.host) {
                         sendError(socket, "Host cannot submit an answer.");
@@ -119,12 +125,12 @@ class teachingGame {
                     }
                     break;
                 case (messages.CONTINUE):
-                    if (this.state === STATES.AWAIT_NEXT && socket === this.host) {
+                    if (this.state === this.STATES.AWAIT_NEXT && socket === this.host) {
                         this.advanceQuestion(socket, message);
-                    } else if (this.state === STATES.FINAL && socket === this.host) {
+                    } else if (this.state === this.STATES.FINAL && socket === this.host) {
                         this.endGame(socket, message);
                     } else if(socket !== this.host) {
-                        sendError(socket, "Only host can contiinue.");
+                        sendError(socket, "Only host can continue.");
                     }
                     else {
                         sendError(socket, "Game has already started.");
@@ -145,7 +151,7 @@ class teachingGame {
      * @param {WebSocket} socket
      */
     join(socket) {
-        socket.handler = this.receiveMessage;
+        socket.handler = this.receiveMessage.bind(this);
 
         // First join = host, later joins = player
         if(this.host === null) {
@@ -161,6 +167,7 @@ class teachingGame {
 
     /**
      * Checks if the game settings
+     * @param {WebSocket} socket Websocket making the request. Used for sending errors
      * @param {Number} num_questions Number of questions in the game
      * @param {Array} categories Array of numbers corresponding to categories included in the game
      * @param {Number} preview_time Time the question text is shown but not answers
@@ -168,8 +175,8 @@ class teachingGame {
      * @param {Number} live_time Time the question is live for answers
      * @returns true/false if the settings are valid
      */
-    validateSettings(num_questions, categories, preview_time, dead_time, live_time) {
-        const valid = true;
+    validateSettings(socket, num_questions, categories, preview_time, dead_time, live_time) {
+        let valid = true;
         if(num_questions > this.MAX_QUESTIONS || num_questions < this.MIN_QUESTIONS) {
             sendError(socket, "Invalid setting: number of questions.");
             valid = false;
@@ -203,7 +210,7 @@ class teachingGame {
             }
         });
         categories.reduce((acc, cur) => {
-            acc[cur] = (acc[curr] || 0) + 1;
+            acc[cur] = (acc[cur] || 0) + 1;
             if(acc[cur] > 1) {
                 sendError(socket, "Invalid setting: categories.");
                 valid = false;
@@ -233,7 +240,7 @@ class teachingGame {
     }
 
     /**
-     * Loads questions into the teaching game
+     * Loads questions into the teaching game (this.questions)
      * @param {Array} categories Array of numbers corresponding to categories included in the game
      * @param {Number} count Number of questions
      */
@@ -294,14 +301,13 @@ class teachingGame {
      * @param {Object} message Message object containing the request
      */
     startGame(socket, message) {
-        // TODO load settings(&validate), get questions, and start
         const num_questions = message.num_questions;
         const categories = message.categories;
         const preview_time = message.preview_time;
         const dead_time = message.dead_time;
         const live_time = message.live_time;
 
-        const valid = this.validateSettings(num_questions, categories, preview_time, dead_time);
+        const valid = this.validateSettings(socket, num_questions, categories, preview_time, dead_time);
         if(!valid) {
             return; // Do nothing
         }
@@ -327,6 +333,7 @@ class teachingGame {
      * See "Teaching Game Flow" https://drive.google.com/file/d/1Ot5iEwynpoNxzL3qfV24YOHXDSmfGL-P/view?usp=sharing
      */
     async serveQuestions() {
+        let rankings = null; // Declare for later use
         while(this.current_question_idx  < this.num_questions) {
             // Send QUESTION to host & players
             let currentMessage = {
@@ -338,7 +345,7 @@ class teachingGame {
             sendWebSocketMessage(this.host, currentMessage);
             this.sendAllPlayers(currentMessage);
 
-            this.state = STATES.SHOW_QUESTION;
+            this.state = this.STATES.SHOW_QUESTION;
 
             // Delay 1 - Show question
             await this.delay(1000 * this.preview_time);
@@ -351,7 +358,7 @@ class teachingGame {
             choices_list.push(this.questions[this.current_question_idx].incorrTWO);
             choices_list.push(this.questions[this.current_question_idx].incorrTHREE);
             this.shuffle(choices_list);
-            const correct_answer_number = choices_list.indexOf(correct_answer) + 1;
+            this.current_correct_answer_number = choices_list.indexOf(correct_answer) + 1;
 
             currentMessage = {
                 "type": messages.CHOICES,
@@ -360,7 +367,7 @@ class teachingGame {
             sendWebSocketMessage(this.host, currentMessage);
             this.sendAllPlayers(currentMessage);
 
-            this.state = STATES.SHOW_ANSWERS;
+            this.state = this.STATES.SHOW_ANSWERS;
 
             // Delay 2 - Show Answers
             await this.delay(1000 * this.dead_time);
@@ -372,50 +379,80 @@ class teachingGame {
             sendWebSocketMessage(this.host, currentMessage);
             this.sendAllPlayers(currentMessage);
 
-            this.state = STATES.RECEIVE_RESPONSES;
+            this.state = this.STATES.RECEIVE_RESPONSES;
 
             // Delay 3 - Accept Answers
+            this.answering_start_time = Date();
             await this.delay(1000 * this.live_time);
 
             // Fill in incorrect response(-1) for players who didn't answer
-            //TODO
-
-            // Calculate points for all players
-            //TODO
+            this.players.forEach((player) => {
+                if(this.answers[player].length < this.current_question_idx + 1) {
+                    this.answers[player][this.current_question_idx] = this.NO_ANSWER_NUM;
+                }
+                // Don't need to add any points
+            });
 
 
             // Send CLOSE
-            const rankings = this.getRankings();
+            rankings = this.getRankings();
             sendWebSocketMessage(this.host, {
                 "type": messages.CLOSE,
-                "correct_answer_number": correct_answer_number,
+                "correct_answer_number": this.current_correct_answer_number,
                 "current_player": null,
                 "other_players": rankings,
             });
             this.players.forEach((player) => {
                 const other_players = [];
-                // TODO left off here
-                // TODO construct other_players
+                this.players.forEach((p) => {
+                    if(p !== player) {
+                        other_players.push(rankings[p]);
+                    }
+                });
 
                 sendWebSocketMessage(player, {
                     "type": messages.CLOSE,
-                    "correct_answer_number": correct_answer_number,
+                    "correct_answer_number": this.current_correct_answer_number,
                     "current_player": rankings[player],
                     "other_players": other_players,
                 });
             });
 
-            this.state = STATES.AWAIT_NEXT;
+            this.state = this.STATES.AWAIT_NEXT;
 
-            // Wait for CONTINUE
-            //TOOD
+            // Wait for CONTINUE (advanceQuestion)
+            await this.waitForContinue();
 
             // Increment question index
             this.current_question_idx = this.current_question_idx + 1;
         }
 
         // Send RESULTS
-        //TODO
+        rankings = this.getRankings();
+            sendWebSocketMessage(this.host, {
+                "type": messages.CLOSE,
+                "correct_answer_number": this.current_correct_answer_number,
+                "current_player": null,
+                "other_players": rankings,
+            });
+            this.players.forEach((player) => {
+                const other_players = [];
+                this.players.forEach((p) => {
+                    if(p !== player) {
+                        other_players.push(rankings[p]);
+                    }
+                });
+
+                sendWebSocketMessage(player, {
+                    "type": messages.CLOSE,
+                    "correct_answer_number": this.current_correct_answer_number,
+                    "current_player": rankings[player],
+                    "other_players": other_players,
+                });
+            });
+
+            this.state = this.STATES.FINAL;
+            // Now wait till CONTINUE to end game
     }
 
     /**
@@ -424,16 +461,51 @@ class teachingGame {
      * @param {Object} message Message object containing the request
      */
     registerAnswer(socket, message) {
-        // TODO
+        const answer_number = message.answer_number;
+
+        // Register answer
+        this.answers[socket][this.current_question_idx] = answer_number;
+        // Add points
+        if (answer_number === this.current_correct_answer_number) {
+            const elapsed_time = this.answering_start_time - date();
+            const elapsed_seconds = (elapsed_time.getMinutes() * 60) + elapsed_time.getSeconds();
+
+            // Points = ratio of elapsed time to live time, multiplied by the base number of points
+            let points = (elapsed_seconds / this.live_time) * this.BASE_QUESTION_POINTS;
+
+            if(points < 0) {
+                // Timed out, no points
+                points = 0;
+                // Set no answer because answer did not come in time
+                this.answers[socket][this.current_question_idx] = this.NO_ANSWER_NUM;
+            }
+
+            this.points[socket] += points;
+        }
     }
 
     /**
-     * Sends out next question.
+     * Called by main flow to wait for CONTINUE signal
+     * @returns A promise waiting for something to call signalContinue()
+     */
+    waitForContinue() {
+        return new Promise(resolve => {
+            this.signalContinue = resolve;
+        });
+    }
+
+    /**
+     * Signals main game flow to send out next question.
      * @param {WebSocket} socket Host websocket which initiated the request
      * @param {Object} message Message object containing the request
      */
     advanceQuestion(socket, message) {
-        // TODO
+        if (this.signalContinue) {
+            this.signalContinue();
+            this.signalContinue = null; // Reset
+        } else {
+            sendError(socket, "Cannot send next question");
+        }
     }
 
     
@@ -444,7 +516,6 @@ class teachingGame {
      * @param {Object} message Message object containing the request
      */
     endGame(socket, message) {
-        this.state = STATES.FINAL;
         // Close host
         sendWebSocketMessage(this.host, {
             "type": messages.DONE,
