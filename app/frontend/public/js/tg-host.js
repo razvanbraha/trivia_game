96195e48c201ada9fcb1f16b96777fc86533941a
Will Mungas <wrmungas@ncsu.edu>
2026-03-25 14:37:03 -0400

    Setup skeleton tg-host script

--- app/frontend/public/js/tg-host.js
+++ app/frontend/public/js/tg-host.js
@@ -11,29 +11,87 @@
 
 import {ws_client} from "../websocket-client.js"
 
-//--- SCRIPT -----------------------------------------------------------
+//--- SETUP -------------------------------------------------------------------
+
+const game_states = {
+    LOBBY: 0,
+    WAITING: 1,
+    QUESTION_SERVED: 2,
+    ANSWERS_SERVED: 3,
+    RESPONSE_LIVE: 4,
+    RESPONSE_SENT: 5,
+    RESPONSE_CLOSED: 6
+};
+
+let current_state = game_states.LOBBY;
+
+const question_html = 
+`
+`;
+
+const answer_html = 
+`
+`;
+
+//--- MAIN SIGNAL HANDLER -----------------------------------------------------
+/**
+ * @author Will Mungas
+ * Handles all incoming (server) websocket signals
+ * 
+ * @param {*} data data received from the websocket
+ */
+function handler(data) {
+    switch(data.type) {
+        case "QUESTION":
+
+        case "CHOICES":
+        
+        case "READY":
+
+        case "CLOSE":
+
+        case "RESULTS":
+
+        case "DONE":
+    }
+};
+
+//--- SPECIFIC SIGNALS --------------------------------------------------------
+
+function handle_question() {
+
+}
+
+function handle_error() {
+
+}
+
+//--- SCRIPT ------------------------------------------------------------------
 
 // first get the code from localStorage
 const code = localStorage.getItem("code");
 if(!code) {
-    // if nothing was stored in localStorage, you have arrived at this page without creating a game
+    // if nothing was stored in localStorage
+    // you have arrived at this page without creating a game
     console.log("unable to join game; have you created one?");
 }
 
 // initiate websocket connection to this code
 const ws = new WebSocket(ws_client.uri);
 
+// setup handlers
 ws_client.init(ws, handler);
 
-const handler = () => {
-
-}
-
+// initiate joining the game as host
 ws.send(JSON.stringify(
     {
-        game_type: ws_client.types.TEACHING,
-        game_code: JSON(code)
+        type: "JOIN",
+        body: {
+            game_type: ws_client.types.TEACHING,
+            game_code: JSON(code)
+        }
     }
-))
+));
 
-// callbacks for websocket signals
+
+
