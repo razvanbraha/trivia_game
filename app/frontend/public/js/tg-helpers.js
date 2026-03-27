4ae19604c017ce8ccc71165de1d61611d43fb000
Connor Hekking <clhekkin@ncsu.edu>
2026-03-27 14:08:44 -0400

    Add leaderboard helper

--- app/frontend/public/js/tg-helpers.js
+++ app/frontend/public/js/tg-helpers.js
@@ -8,9 +8,9 @@ const template_str = await fetch('/public/templates/question-template.html').the
 const template_div = document.createElement('div');
 template_div.innerHTML = template_str;
 const template = template_div.querySelector("#game-ui-template");
-const base_question_container = template.content.querySelector("#question-container").cloneNode(true);
+const template_question_container = template.content.querySelector("#question-container").cloneNode(true);
 if(window.location.href.includes("test-tg-templates")) {
-    document.body.appendChild(base_question_container);
+    document.body.appendChild(template_question_container);
 }
 
 
@@ -24,11 +24,11 @@ if(window.location.href.includes("test-tg-templates")) {
  */
 function createShowQuestion(questionText) {
     // Create new empty instance of question_container
-    const question_container = base_question_container.cloneNode(false);
+    const question_container = template_question_container.cloneNode(false);
 
     // Clone sub elements
-    const next_question = base_question_container.querySelector(".next-question").cloneNode(true);
-    const question_text = base_question_container.querySelector(".question-text").cloneNode(true);
+    const next_question = template_question_container.querySelector(".next-question").cloneNode(true);
+    const question_text = template_question_container.querySelector(".question-text").cloneNode(true);
 
     // Edit sub elements
     question_text.querySelector('p').innerText = questionText;
@@ -58,12 +58,12 @@ function createShowAnswers(questionText, answers, timerStart) {
     }
 
     // Create new empty instance of question_container
-    const question_container = base_question_container.cloneNode(false);
+    const question_container = template_question_container.cloneNode(false);
     
     // Clone sub elements
-    const question_text = base_question_container.querySelector(".question-text").cloneNode(true);
-    const question_choices = base_question_container.querySelector(".question-choices").cloneNode(true);
-    const countdown = base_question_container.querySelector(".countdown").cloneNode(true);
+    const question_text = template_question_container.querySelector(".question-text").cloneNode(true);
+    const question_choices = template_question_container.querySelector(".question-choices").cloneNode(true);
+    const countdown = template_question_container.querySelector(".countdown").cloneNode(true);
 
     // Edit sub elements
     question_text.querySelector('p').innerText = questionText;
@@ -104,11 +104,11 @@ function showCorrectAnswer(chosenAnswerNum, correctAnswerNum, isTeacher) {
     if(!isTeacher) {
         if(chosenAnswerNum == correctAnswerNum) {
             // Add correct element
-            const correct_prompt = base_question_container.querySelector(".correct-prompt").cloneNode(true);
+            const correct_prompt = template_question_container.querySelector(".correct-prompt").cloneNode(true);
             question_container.appendChild(correct_prompt);
         } else {
             // Add incorrect element
-            const incorrect_prompt = base_question_container.querySelector(".incorrect-prompt").cloneNode(true);
+            const incorrect_prompt = template_question_container.querySelector(".incorrect-prompt").cloneNode(true);
             question_container.appendChild(incorrect_prompt);
         }
     } else {
@@ -138,11 +138,84 @@ function showCorrectAnswer(chosenAnswerNum, correctAnswerNum, isTeacher) {
  * 
  * Returns the leaderboard page object, which can then be cloned
  * 
- * @param {Number} place Current player's place
- * @param {*} statistics Statistics to display(format unknown currently)
+ * @param {{points: Number, rank: Number}} current_player Current player's points and rank in an object
+ * @param {Array({points: Number, rank: Number})} other_players Array of other player's points and rank in an object
  * @param {Boolean} isTeacher If the page object should be prepared for a teacher instead of a student view
  * @returns cloneable object containing the body of the leaderboard page 
  */
 function createLeaderboard(place, statistics, isTeacher) {
-    //TODO
+    // Copy statistics list and insert player
+    const players_list = other_players.slice().push(current_player);
+    // Sort player list
+    players_list.sort((a, b) => b.points - a.points);
+
+    // Create new empty instance of question_container
+    const question_container = template_question_container.cloneNode(false);
+    
+    // Clone sub elements
+    const self_ranking = template_question_container.querySelector(".self-ranking").cloneNode(true);
+    const leaderboard = template_question_container.querySelector(".leaderboard").cloneNode(true);
+
+    // Edit sub elements
+    let place_text;
+    let encouragement_text;
+    switch (current_player.rank) {
+        case 1:
+            place_text = '1st place!';
+            encouragement_text = 'On top of the world!';
+            break;
+        case 2:
+            place_text = '2nd place!';
+            encouragement_text = 'Almost perfect!';
+            break;
+        case 3:
+            place_text = '3rd place!';
+            encouragement_text = 'Nearly there!';
+            break;
+        case 4:
+            place_text = '4th place!';
+            encouragement_text = 'Keep up the lead!';
+            break;
+        case 5:
+            place_text = '5th place!';
+            encouragement_text = 'Keep up the lead!';
+            break;
+        default:
+            place_text = `${current_player.rank}th place`;
+            encouragement_text = 'Nice work!';
+            break;
+    }
+    self_ranking.querySelectorAll('p')[0].innerText = `You are in ${place_text}`;
+    self_ranking.querySelectorAll('p')[1].innerText = encouragement_text;
+    let idx = 0;
+    leaderboard.querySelectorAll('p').forEach((ranking) => {
+        ranking.innerText = `Player: ${players_list[idx].points}`;
+        idx += 1;
+    });
+
+    // Add sub elements
+    question_container.appendChild(self_ranking);
+    question_container.appendChild(leaderboard);
+    if(isTeacher) {
+        const next_question_btn = template_question_container.querySelector(".next-question-btn").cloneNode(true);
+        question_container.appendChild(next_question_btn);
+    }
+
+    // Return cloneable obj
+    return question_container;
+}
+
+/**
+ * @author Connor Hekking
+ * 
+ * Returns the ENDING leaderboard page object, which can then be cloned
+ * 
+ * @param {{points: Number, rank: Number}} current_player Current player's points and rank in an object
+ * @param {Array({points: Number, rank: Number})} other_players Array of other player's points and rank in an object
+ * @param {Boolean} isTeacher If the page object should be prepared for a teacher instead of a student view
+ * @returns cloneable object containing the body of the leaderboard page 
+ */
+function createEndLeaderboard(place, statistics, isTeacher) {
+    //TODO this is a placeholder, need to make distinct end leaderboard
+    return createLeaderboard(place, statistics, isTeacher);
 }
