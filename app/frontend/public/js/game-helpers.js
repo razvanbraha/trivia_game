cda5181a658dc95d498919c08d779aed92f7e392
Will Mungas <wrmungas@ncsu.edu>
2026-03-30 16:18:18 -0400

    separate out lobby page creation and players list creation

--- app/frontend/public/js/game-helpers.js
+++ app/frontend/public/js/game-helpers.js
@@ -36,54 +36,57 @@ function clearContent() {
  * @author Will Mungas
  * @description Creates a lobby page within the content element, listing the
  * settings and the currently joined players
- * @param {*} players 
+ * @param {*} players list of player names
+ * @param {(name: String)} kick function to call to kick a player by name (called when button is clicked)
  */
-function createLobby(players) {
+function createLobby(code) {
     const content = getContent();
 
     clearContent();
-    createSettings();
-    createPlayers(players);
+
+    // sets up lobby: added to page content when the teacher first joins the game successfully
+    // game-players is dynamically updated by the teacher script (tg-host.js) on JOINEE and KICK events
+    content.innerHTML += 
+    `
+    <section id="game-settings">
+        <h3>JOIN: ${code}</h3>
+        <h4>Settings</h4>
+        <p>TODO: add other settings</p>
+    </section>
+    <section id="game-players">
+        <h4>Joined Players</h4>
+        <div id="players-list">
+            TODO add players
+        </div>
+        <button id="start-game">Start Game</button>
+    </section>
+    `;
 
     content.classList.add("lobby-ctnr")
 }
 
-/**
- * @author Will Mungas
- * @description Creates a settings pane inside the content element
- */
-function createSettings() {
-    const content = getContent();
 
-    content.innerHTML += 
-    `
-    <section id="game-settings">
-        TODO: add settings
-    </section>
-    `;
-}
-
-/**
- * @author Will Mungas
- * @description creates a players pane inside the content element
- * @param {*} players 
- */
-function createPlayers(players) {
-    const content = getContent();
-
-    content.innerHTML += 
-    `
-    <section id="game-players">
-        <h4>Joined Players</h4>
-        <div id="players-list">
-            TODO: add players
+function updatePlayers(players, kick) {
+    const player_section = document.getElementById("players-list");
+    player_section.innerHTML = "";
+    for(const player of players) {
+        player_section.innerHTML += 
+        `
+        <div>
+            <p>${player}</p>
+            <button>Kick</button>
         </div>
-    </section>
-    `;
-
-    //TODO: loop over players list and add players to the players section,
-    // with "kick" buttons
-
+        `;
+    }
+    // add functionality to kick buttons
+    const player_elements = player_section.querySelectorAll("div");
+    for(const player of player_elements) {
+        const text = player.querySelector("p").innerText;
+        const kick_btn = player.querySelector("button");
+        kick_btn.onclick = () => {
+            kick(text);
+        }
+    }
 }
 
 // TODO add functions to create question text, create answer choices, etc
@@ -93,5 +96,6 @@ function createPlayers(players) {
 export default {
     getContent,
     clearContent,
-    createLobby
+    createLobby,
+    updatePlayers
 }
