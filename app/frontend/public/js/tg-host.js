38e91fe66a562d22ea3179359b85f8d5a432c354
Will Mungas <wrmungas@ncsu.edu>
2026-04-16 09:38:32 -0400

    fix websocket connection through shib

--- app/frontend/public/js/tg-host.js
+++ app/frontend/public/js/tg-host.js
@@ -174,8 +174,7 @@ fetch("/api/games", fetchData)
         }
 
         // initiate websocket connection to this code
-        const protocol = window.location.protocol === "https" ? "wss" : "ws";
-        const ws_url = `${protocol}://${window.location.host}:${window.location.port}`;
+        const ws_url = `wss://${window.location.host}/api/`;
         console.log(`Game created with code ${code}; initiating Websocket connection to ${ws_url}`);
         ws = new WebSocket(ws_url);
 
